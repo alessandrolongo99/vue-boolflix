2f78bdbcfe8a4cd9a19d627e712b28f8c40caa2e
alessandrolongo99 <longoalessandro153@gmail.com>
2022-07-21 15:26:13 +0200

    milestone 2: flag language

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,56 @@
 <template>
   <div id="app">
-    <AppHeader/>
-    <input type="text" v-model="name">
+    <AppHeader />
+    <input type="text" v-model="name" />
     <button @click="searchName">Search</button>
+    <h2>FILM</h2>
     <div class="card" v-for="(movie, index) in movies" :key="index">
-      <div>Titolo: {{movie.title}}</div>
-      <div>Titolo originale: {{movie.original_title}}</div>
-      <div>Lingua originale: {{movie.original_language}}</div>
-      <div>Voto: {{movie.vote_average}}</div>
+      <div>Titolo: {{ movie.title }}</div>
+      <div>Titolo originale: {{ movie.original_title }}</div>
+      <div>
+        Lingua originale:
+        <img :src="getFlag(movie.original_language)" alt="" />
+      </div>
+      <div>Voto: {{ movie.vote_average }}</div>
     </div>
   </div>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue'
-import axios from 'axios'
+import AppHeader from "./components/AppHeader.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     AppHeader,
   },
-  data: function(){
-    return{
+  data: function () {
+    return {
       movies: [],
-      name: '',
-    }
+      name: "",
+    };
   },
   methods: {
-    searchName(){
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=666c60c46937c74d09eb7327646c579d&language=it-IT&query=${this.name}`)
-      .then((results) =>{      
+    searchName() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=666c60c46937c74d09eb7327646c579d&language=it-IT&query=${this.name}`
+        )
+        .then((results) => {
           this.movies = results.data.results;
-          console.log(this.movies)
-      })
+          console.log(this.movies);
+        });
     },
-  }
+    getFlag(language) {
+      return `https://www.unknown.nu/flags/images/${language}-100`;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-.card{
+.card {
   padding: 1rem 0;
 }
 </style>
